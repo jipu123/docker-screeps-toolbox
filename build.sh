a0cd7c7682c138e1efa6f37cf4e1e7886849afb6
huang puguang <puguang_huang@fpi-inc.com>
2019-08-01 16:10:07 +0800

    fix: add respberrypi Dockerfile

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
-version=$(curl https://raw.githubusercontent.com/screeps/screeps/master/package.json | jq -r .version)
-git clone git@github.com:ags131/docker-screeps-server
-pushd docker-screeps-server
-sed -i 's/^ENV SCREEPS_VERSION.*$/ENV SCREEPS_VERSION '$version'/i' Dockerfile
-git commit -a -m "Auto Update to screeps "$version
-git push origin master --tags
-popd
-rm -rf docker-screeps-server
+#! /bin/bash
+
+docker build -t screeps-server .
